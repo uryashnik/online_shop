633bf0650ad9c69b7152990397153152a113cce8
dd310791uvv <Dsf3f3f3@@>
2022-03-22 19:51:20 +0200

    refactor controllers

--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
-const models = require('./models/model');
+const models = require('./models/models');
 const cors = require('cors');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
-app.use(express.json()); // чтобы приложение могло парсить json формат
+app.use(express.json()); // чтобы приложение могло парсить json формат и видеть req.body
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello!!!'})
-});
+app.use(errorHandler);
 
 const start = async () => {
     try{
